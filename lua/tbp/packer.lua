9d9ea4a526df757344b12e55084d51d80a342bfd
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-11-09 22:21:54 +0900

    Easymotion added

--- lua/tbp/packer.lua
+++ lua/tbp/packer.lua
@@ -4,6 +4,7 @@ return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 	use("christoomey/vim-tmux-navigator")
+	use("easymotion/vim-easymotion")
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.4",
