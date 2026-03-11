825c9638b526f519b8bfbe4552e4e03ce9976d3b
Thomas <barrepitousthomas@gmail.com>
2026-03-11 14:57:39 +0100

    Added render markdown extension

--- lua/tbp/packer.lua
+++ lua/tbp/packer.lua
@@ -69,6 +69,16 @@ return require("packer").startup(function(use)
             })
         end,
     }
+    use({
+        'MeanderingProgrammer/render-markdown.nvim',
+        after = { 'nvim-treesitter' },
+        requires = { 'nvim-mini/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+        -- requires = { 'nvim-mini/mini.icons', opt = true }, -- if you use standalone mini plugins
+        -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+        config = function()
+            require('render-markdown').setup({})
+        end,
+    })
 
     -- Spell checker
     use {
